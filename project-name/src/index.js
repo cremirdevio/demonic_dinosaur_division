979cf5455a80709388ef166c44e698f71687b7fe
DmitriyR54 <96314763+DmitriyR54@users.noreply.github.com>
2022-08-24 18:58:30 +0300

    Initial commit

---
+++ project-name/src/index.js
@@ -0,0 +1,12 @@
+// libraries
+import 'core-js/actual';
+import _ from 'lodash';
+// sections
+import TestComponent from 'Sections/TestSection/TestComponent';
+// components
+import LazyLoad from 'Components/LazyLoad/LazyLoad';
+// styles
+import './styles/styles.scss';
+
+TestComponent();
+LazyLoad();
