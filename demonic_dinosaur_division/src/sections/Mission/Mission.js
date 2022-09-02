3485ef24b452c83c41c2658fc9402d862edf248c
DmitriyR54 <45ryirtimd@gmail.com>
2022-09-03 00:23:55 +0300

    mission section chart parts animation added

--- demonic_dinosaur_division/src/sections/Mission/Mission.js
+++ demonic_dinosaur_division/src/sections/Mission/Mission.js
@@ -29,6 +29,45 @@ const MissionSection = (container) => {
             missionChart.style.transform = 'translateX(-50%) scale(1)';
             missionChart.style.opacity = '1';
         }
+
+        /* animate parts of the chart */
+        const missionChartParts = document.querySelectorAll('.mission__chart-pie');
+
+        const activeChartPie = (item) => {
+            missionChartParts.forEach((chartPart) => {
+                /* remove the class name from an active element */
+                chartPart.classList.remove('mission__chart-pie-active');
+            });
+            /* add active class to the element that has been hovered on to */
+            item.classList.add('mission__chart-pie-active');
+        };
+
+        missionChartParts.forEach((item) => {
+            item.addEventListener('mouseenter', () => {
+                activeChartPie(item);
+            });
+
+            /* remove the class name from an active element */
+            item.addEventListener('mouseleave', () => {
+                item.classList.remove('mission__chart-pie-active');
+            });
+        });
+
+        /* add an effect when user hovers over the description text */
+        const chartDescriptionItems = document.querySelectorAll('.distributions__chart-item');
+
+        chartDescriptionItems.forEach((item, index) => {
+            const respectivelyChartPart = missionChartParts[index];
+
+            item.addEventListener('mouseenter', () => {
+                activeChartPie(respectivelyChartPart);
+            });
+
+            /* remove the class name from an active element */
+            item.addEventListener('mouseleave', () => {
+                respectivelyChartPart.classList.remove('mission__chart-pie-active');
+            });
+        });
     });
 };
 
